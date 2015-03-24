f3349794db184116aad45f3b54c0f2e1d270b965
Hazow <gautreau.anthony@gmail.com>
2015-03-24 13:13:40 +0100

    no message

---
+++ app/src/main/java/suptracking/supinfo/com/suptracking/Login.java
@@ -0,0 +1,65 @@
+package suptracking.supinfo.com.suptracking;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import org.json.JSONObject;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+public class Login extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        HttpClient client = new DefaultHttpClient();
+        HttpPost post = new HttpPost("http://91.121.105.200/SUPTracking/");
+
+        /*JSONObject jsonobj = new JSONObject();
+        jsonobj.put("email", "a@b.com");
+        jsonobj.put("old_passw", "306");
+        jsonobj.put("use_id", "123");
+        jsonobj.put("new_passw", "456");*/
+
+        Button butSignIn = (Button) findViewById(R.id.btnSingIn);
+
+        butSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
